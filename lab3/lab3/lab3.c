13cfe02dcfbb1be77b45bc4cf7885be191ff6af9
Alexandr022 <112651706+Alexandr022@users.noreply.github.com>
2023-05-20 19:39:00 +0300

    Update lab3.c

--- lab3/lab3/lab3.c
+++ lab3/lab3/lab3.c
@@ -100,30 +100,27 @@ void convertToBlackAndWhite(BMPfile BMP)
 void medianFilter(BMPfile* BMP, int filterSize) {
     int pad = (4 - ((BMP->BMPinfo.width * BMP->BMPinfo.bitsPixels / 8) % 4)) % 4;
     unsigned char* tempPixels = (unsigned char*)malloc(BMP->BMPinfo.width * BMP->BMPinfo.height * BMP->BMPinfo.bitsPixels / 8);
-
     
+    // Copy pixel data to tempPixels
     for (int i = 0; i < BMP->BMPinfo.width * BMP->BMPinfo.height * BMP->BMPinfo.bitsPixels / 8; i++) {
         tempPixels[i] = BMP->pixels[i];
     }
-
+    
     for (unsigned int y = 0; y < BMP->BMPinfo.height; y++) {
         for (unsigned int x = 0; x < BMP->BMPinfo.width; x++) {
             int red = 0;
             int green = 0;
             int blue = 0;
             int count = 0;
-
+            
             for (int fy = -filterSize / 2; fy <= filterSize / 2; fy++) {
                 for (int fx = -filterSize / 2; fx <= filterSize / 2; fx++) {
                     int xi = x + fx;
                     int yi = y + fy;
-
                     if (xi < 0 || xi >= BMP->BMPinfo.width || yi < 0 || yi >= BMP->BMPinfo.height) {
                         continue;
                     }
-
                     int index = ((BMP->BMPinfo.height - yi - 1) * BMP->BMPinfo.width + xi) * BMP->BMPinfo.bitsPixels / 8;
-
                     if (index + 2 < BMP->BMPinfo.width * BMP->BMPinfo.height * BMP->BMPinfo.bitsPixels / 8) {
                         int b = tempPixels[index];
                         int g = tempPixels[index + 1];
@@ -135,14 +132,14 @@ void medianFilter(BMPfile* BMP, int filterSize) {
                     }
                 }
             }
-
             int index = ((BMP->BMPinfo.height - y - 1) * BMP->BMPinfo.width + x) * BMP->BMPinfo.bitsPixels / 8;
-            BMP->pixels[index] = (unsigned char)(blue / count);
-            BMP->pixels[index + 1] = (unsigned char)(green / count);
-            BMP->pixels[index + 2] = (unsigned char)(red / count);
+            if (count > 0) {
+                BMP->pixels[index] = (unsigned char)(blue / count);
+                BMP->pixels[index + 1] = (unsigned char)(green / count);
+                BMP->pixels[index + 2] = (unsigned char)(red / count);
+            }
         }
     }
-
     free(tempPixels);
 }
 
