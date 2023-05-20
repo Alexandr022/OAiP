41a5f4148447d91d045740d2dfe4138369665c7a
Alexandr022 <112651706+Alexandr022@users.noreply.github.com>
2023-05-20 20:28:50 +0300

    Update lab2.2.c

--- lab2/lab2.2/lab2.2/lab2.2.c
+++ lab2/lab2.2/lab2.2/lab2.2.c
@@ -3,109 +3,99 @@
 
 #include "lab2.2.h"
 
-int replaysment(FILE* file, Words* wordsArrOne, Words* wordsArrTwo, int size, char* word)
-{
-	int bool_ = 0;
+int replacement(FILE* file, Words* wordsArrOne, Words* wordsArrTwo, int size, char* word) {
+    int bool_ = 0;
 
-	for (int i = 0; i < size; i++)
-	{
-		if (!strcmp(wordsArrOne[i].word, word))
-		{
-			fprintf(file, "%s", wordsArrTwo[i].word);
-			bool_ = 1;
-		}
-		else if (!strcmp(wordsArrTwo[i].word, word))
-		{
-			fprintf(file, "%s", wordsArrOne[i].word);
-			bool_ = 1;
-		}
-	}
-	return bool_;
+    for (int i = 0; i < size; i++) {
+        if (!strcmp(wordsArrOne[i].word, word)) {
+            fprintf(file, "%s", wordsArrTwo[i].word);
+            bool_ = 1;
+        } else if (!strcmp(wordsArrTwo[i].word, word)) {
+            fprintf(file, "%s", wordsArrOne[i].word);
+            bool_ = 1;
+        }
+    }
+    return bool_;
 }
 
-void initializeArr(FILE* file, Words** wordsArrayOne, Words** wordsArrayTwo, int* size, Words* words)
-{
-	for (int i = 0; !feof(file); i++)
-	{
-		char* word1 = malloc(100);
-		char* word2 = malloc(100);
+void initializeArr(FILE* file, Words** wordsArrayOne, Words** wordsArrayTwo, int* size) {
+    int i = 0;
+    while (!feof(file)) {
+        char* word1 = malloc(100);
+        char* word2 = malloc(100);
+        fscanf(file, "%s", word1);
+        fscanf(file, "%s", word2);
 
-		fscanf_s(file, "%s", word1, 100);
+        (*wordsArrayOne)[i] = (Words){0};
+        (*wordsArrayTwo)[i] = (Words){0};
 
-		if (strcmp(word1, "<!>") == 0)
-		{
-			(*wordsArrayOne) = realloc(*wordsArrayOne, (--(*size)) * sizeof(Words));
-			(*wordsArrayTwo) = realloc(*wordsArrayTwo, (*size) * sizeof(Words));
-			break;
-		}
-		fscanf_s(file, "%s", word2, 100);
+        strcpy((*wordsArrayOne)[i].word, word1);
+        strcpy((*wordsArrayTwo)[i].word, word2);
 
-		(*wordsArrayOne)[(*size) - 1].word = malloc(100);
-		(*wordsArrayOne)[(*size) - 1].word = word1;
+        (*size)++;
+        (*wordsArrayOne) = realloc(*wordsArrayOne, (*size) * sizeof(Words));
+        (*wordsArrayTwo) = realloc(*wordsArrayTwo, (*size) * sizeof(Words));
 
-		(*wordsArrayTwo)[(*size) - 1].word = malloc(100);
-		(*wordsArrayTwo)[(*size) - 1].word = word2;
-
-		(*wordsArrayOne) = realloc(*wordsArrayOne, (++(*size)) * sizeof(Words));
-		(*wordsArrayTwo) = realloc(*wordsArrayTwo, (*size) * sizeof(Words));
-
-		printf("%d: %s -> %s\n", i + 1, (*wordsArrayOne)[i].word, (*wordsArrayTwo)[i].word);
-	}
+        printf("%d: %s -> %s\n", i + 1, (*wordsArrayOne)[i].word, (*wordsArrayTwo)[i].word);
+        i++;
+        free(word1);
+        free(word2);
+    }
 }
 
-void fillFile(FILE* file, FILE* newFile, Words* wordsArrayOne, Words* wordsArrayTwo, int size)
-{
-	for (int i = 0; !feof(file); i++)
-	{
-		char c;
-		char* word = malloc(100);
-		fscanf_s(file, "%s%c", word, 100, &c, 1);
-		if (!replaysment(newFile, wordsArrayOne, wordsArrayTwo, size, word) && !feof(file))
-		{
-			fprintf(newFile, "%s", word);
-		}
-		if (!feof(file)) fprintf(newFile, "%c", c);
-		free(word);
-	}
+void fillFile(FILE* file, FILE* newFile, Words* wordsArrayOne, Words* wordsArrayTwo, int size) {
+    int i = 0;
+    while (!feof(file)) {
+        char c;
+        char* word = malloc(100);
+        fscanf(file, "%s%c", word, &c);
+        if (!replacement(newFile, wordsArrayOne, wordsArrayTwo, size, word) && !feof(file)) {
+            fprintf(newFile, "%s", word);
+        }
+        if (!feof(file)) {
+            fprintf(newFile, "%c", c);
+        }
+        free(word);
+    }
 }
 
-void displaySize(FILE* inputFilename, FILE* outputFilename)
-{
-	fseek(inputFilename, 0, SEEK_END);
-	long inputSize = ftell(inputFilename);
-
-	fseek(outputFilename, 0, SEEK_END);
-	long outputSize = ftell(outputFilename);
-
-	printf("Input file size: %ld bytes\n", inputSize);
-	printf("Output file size: %ld bytes\n", outputSize);
+void displaySize(FILE* inputFilename, FILE* outputFilename) {
+    fseek(inputFilename, 0, SEEK_END);
+    long inputSize = ftell(inputFilename);
+    fseek(outputFilename, 0, SEEK_END);
+    long outputSize = ftell(outputFilename);
+    printf("Input file size: %ld bytes\n", inputSize);
+    printf("Output file size: %ld bytes\n", outputSize);
 }
 
-int main()
-{
-	FILE* filename;
-	FILE* outputFilename;
-	fopen_s(&outputFilename, "C:/Users/Asus/Desktop/lab2/decompressed.txt", "w+");
+int main() {
+    FILE* filename;
+    FILE* outputFilename;
 
-	if (fopen_s(&filename, "C:/Users/Asus/Desktop/lab2/compressed.txt", "r") != 0)
-	{
-		printf("File is not open or does not exist!!!\n");
-	}
+    if (fopen_s(&filename, "C:/Users/Asus/Desktop/lab2/compressed.txt", "r") != 0) {
+        printf("File is not open or does not exist!!!\n");
+        return 1;
+    }
 
-	int size = 1;
-	Words* words = (Words*)malloc(size * sizeof(Words));
-	Words* wordsOne = (Words*)malloc(size * sizeof(Words));
-	Words* wordsTwo = (Words*)malloc(size * sizeof(Words));
+    fopen_s(&outputFilename, "C:/Users/Asus/Desktop/lab2/decompressed.txt", "w+");
 
-	initializeArr(filename, &wordsOne, &wordsTwo, &size, words);
-	fillFile(filename, outputFilename, wordsOne, wordsTwo, size);
-	displaySize(filename, outputFilename);
+    int size = 0;
+    Words* words = malloc(sizeof(Words));
+    Words* wordsOne = malloc(sizeof(Words));
+    Words* wordsTwo = malloc(sizeof(Words));
 
-	free(wordsOne);
-	free(wordsTwo);
+    initializeArr(filename, &wordsOne, &wordsTwo, &size);
 
-	fclose(filename);
-	fclose(outputFilename);
+    fillFile(filename, outputFilename, wordsOne, wordsTwo, size);
+    displaySize(filename, outputFilename);
+
+    free(wordsOne);
+    free(wordsTwo);
+    free(words);
+    fclose(filename);
+    fclose(outputFilename);
+
+    return 0;
 }
 
 
