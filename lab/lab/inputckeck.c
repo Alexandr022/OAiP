eac7e83c1c5366b56a54345a549fd2d7c0d856e3
Alexandr022 <112651706+Alexandr022@users.noreply.github.com>
2023-05-23 21:59:22 +0300

    Update inputckeck.c

--- lab/lab/inputckeck.c
+++ lab/lab/inputckeck.c
@@ -1,1 +1,43 @@
+#include "lab.h"
+
+int validateInputRange(int min, int max)
+{
+    int value = 0;
+    scanf_s("%d", &value);
+    while (value < min || value > max)
+    {
+        printf("Wrong input. Please enter a value between %d and %d.\n", min, max);
+        rewind(stdin);
+        scanf_s("%d", &value);
+    }
+    return value;
+}
+
+int validateInputValues(int value1, int value2, int value3)
+{
+    int value = 0;
+    scanf_s("%d", &value);
+    while (value != value1 && value != value2 && value != value3)
+    {
+        printf("Wrong input. Please enter a valid value.\n");
+        rewind(stdin);
+        scanf_s("%d", &value);
+    }
+    return value;
+}
+
+int validateInputValues(int value1, int value2, int value3, int value4)
+{
+    int value = 0;
+    scanf_s("%d", &value);
+    while (value != value1 && value != value2 && value != value3 && value != value4)
+    {
+        printf("Wrong input. Please enter a valid value.\n");
+        rewind(stdin);
+        scanf_s("%d", &value);
+    }
+    return value;
+}
+
+
 
