5a88a51f21c86d685fe62002e882c580e6615cad
Alexandr022 <112651706+Alexandr022@users.noreply.github.com>
2023-05-23 22:57:55 +0300

    Update average.c

--- lab2/lab2.1/lab2.1/average.c
+++ lab2/lab2.1/lab2.1/average.c
@@ -1,1 +1,47 @@
 #include "lab2.1.h"
+
+int averageCount(Words* words, int size)
+{
+    int average = 0;
+
+    if (words == NULL)
+    {
+        printf("Invalid pointer: words is NULL.\n");
+        return 0;
+    }
+
+    if (size == 0)
+    {
+        printf("Invalid size: size is zero.\n");
+        return 0;
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        average += words[i].countWords;
+    }
+
+    return (average / size);
+}
+
+int averageLength(Words* words, int size)
+{
+    int average = 0;
+    if (words == NULL)
+    {
+        printf("Invalid pointer: words is NULL.\n");
+        return 0;
+    }
+
+    if (size == 0)
+    {
+        printf("Invalid size: size is zero.\n");
+        return 0;
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        average += words[i].wordSize;
+    }
+    return (average / size);
+}
